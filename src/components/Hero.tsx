0d11c5e7b6066fe62868b1907c15fd6bd4097b56
victorqnk <victor@hazmiapp.com>
2024-10-30 11:03:58 -0600

    feat: setup tailwind and hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+export const Hero = () => {
+  return (
+    <div className='bg-hero h-screen max-w-full bg-cover bg-center bg-no-repeat'>
+      <div className='absolute bottom-20 left-12'>
+        <h1 className='text-5xl text-yellow-400 font-thin'>
+          Hello, <span className='text-cyan-400'>I'm Victor</span>
+        </h1>
+        <p className='text-white text-lg w-96 mt-5'>
+          A full-stack developer with a focus on creating user-centered scalable
+          web and mobile applications with modern technologies.
+        </p>
+      </div>
+    </div>
+  )
+}
