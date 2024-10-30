7c4821d85c9da0be62a4a8e80a2d48399d649dfb
victorqnk <victor@hazmiapp.com>
2024-10-30 12:26:44 -0600

    modify construction

--- src/components/Construction.tsx
+++ src/components/Construction.tsx
@@ -1,14 +1,7 @@
 export const ConstructionBanner = () => {
   return (
-    <div className='absolute top-12 left-0'>
-      <iframe
-        src='https://giphy.com/embed/7vdlbr6lcVAmGK7lhB'
-        width='100%'
-        height='100%'
-      ></iframe>
-      <p className='text-cyan-400 text-center uppercase font-semibold text-xs mt-2'>
-        - Under construction -
-      </p>
-    </div>
+    <p className='text-red-400 uppercase font-semibold text-xs mt-4 animate-pulse'>
+      - still a work in progress -
+    </p>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,15 +3,16 @@ import { ConstructionBanner } from './Construction'
 export const Hero = () => {
   return (
     <div className='bg-hero h-screen max-w-full bg-cover bg-center bg-no-repeat'>
-      <ConstructionBanner />
       <div className='absolute bottom-20 left-12'>
         <h1 className='text-5xl text-yellow-400 font-thin'>
           Hello, <span className='text-cyan-400'>I'm Victor</span>
         </h1>
         <p className='text-white text-lg w-96 mt-5'>
-          A full-stack developer with a focus on creating user-centered scalable
-          web and mobile applications with modern technologies.
+          A full-stack developer with a focus on creating{' '}
+          <span className='text-yellow-400'>user-centered</span> scalable web
+          and mobile applications with modern technologies.
         </p>
+        <ConstructionBanner />
       </div>
     </div>
   )
